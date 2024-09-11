e9eda8ec0b8a744bd9e7fd9db9a13c5ad421a793
ozan1535 <ozanbilgic1535@gmail.com>
2024-09-11 14:49:08 +0300

    Comparison operators

---
+++ script.js
@@ -0,0 +1,39 @@
+// Equal to ==
+/* let isEqual = 5 == "6";
+console.log(isEqual);
+ */
+
+/* // Strict equal ===
+let isStrictEqual = 5 === "5";
+console.log(isStrictEqual);
+ */
+
+/* // Not equal !=
+let isNotEqual = 5 != 5;
+console.log(isNotEqual);
+ */
+
+/* // Strict not equal !==
+let isStrictNotEqual = 5 !== "5";
+console.log(isStrictNotEqual);
+ */
+
+/* // Greater than >
+let isGreater = 10 > 15;
+console.log(isGreater);
+ */
+
+/* // Less than <
+let isLess = 14 < 8;
+console.log(isLess);
+ */
+
+/* // Greater than or equal to >=
+let isGreaterOrEqual = 8 >= 6;
+console.log(isGreaterOrEqual);
+ */
+
+/* // Less than or equal to <=
+let isLessOrEqual = 1 <= 3;
+console.log(isLessOrEqual);
+ */
